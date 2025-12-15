d5b3050933b5dcb0940b53b758bbd4826dab6741
Braxton Wilcken-Pond <a02498177@aggies.usu.edu>
2025-12-15 16:46:08 -0700

    Update on Sports Page

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ import { getMonitor } from './monitoring/dataMonitor.js';
 import ReportEmailService from './reportEmailService.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
